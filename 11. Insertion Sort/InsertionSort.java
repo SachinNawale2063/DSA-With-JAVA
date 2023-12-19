ccaf24d110b9fa83c80761566ccf15b6583d7c22
Sachin Nawale <sachin.nawale2063@gmail.com>
2023-12-19 14:41:47 +0530

    Creating Insertion.sort

--- 11. Insertion Sort/InsertionSort.java
+++ 11. Insertion Sort/InsertionSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = { 3, 1, 5, 4, 2 };
@@ -8,6 +10,16 @@ public class InsertionSort {
     }
 
     static void insertion(int[] arr){
-
+        for(int i = 0; i < arr.length-1; i++){
+            for (int j = i +1; j>0; j--){
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
     }
 }
